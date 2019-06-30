7ddb87e807f1ab9c44de064c6d411e18f0368806
git <820472580@qq.com>
2019-06-30 11:43:09 +0800

    update html file

--- Atguigu_Vue_Fundamental/basic/14.js
+++ Atguigu_Vue_Fundamental/basic/14.js
@@ -1,6 +1,7 @@
 (function () {
-
+    // 需要向外暴露的插件对象
     const MyPlugin = {}
+    // 插件对象必须有一个install属性
     MyPlugin.install = function (Vue, options) {
         // 1. 添加全局方法或属性
         Vue.myGlobalMethod = function () { };
